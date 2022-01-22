87797ee16f9c1dcd0cddbf5a5f783658b5f5de91
Managementaccount <97501211+Managementaccount@users.noreply.github.com>
2022-01-21 20:04:50 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function ADMIN_AUTH() {
+var pin = prompt("WARNING" + "<br/>" + "you are about to enter an administrative environment" + "<br/>" + "if you are here by accident press cancel" + "<br/>" + "otherwise enter the administrative pin"
+if (pin == "7353") {
+alert("welcome admin")
+}
+}
